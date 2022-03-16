1920e4839485bbe22f32296eda2fad18b5ca7ca7
mohammaddavoodi7 <mohammaddavoodi107@gmil.com>
2022-03-17 02:11:44 +0330

    add Media in urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,8 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    #path('blog/', include('blog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
